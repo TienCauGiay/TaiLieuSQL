7cb2033c9bf8630455ac6de488a2e2f76e525c53
Tien <tiensd17052001@gmail.com>
2022-10-25 12:32:18 +0700

    update

---
+++ ThiThu.sql
@@ -0,0 +1,95 @@
+﻿--Thời gian 60’: Cho CSDL về quản lý bán sách:
+use BT1CSDL
+go
+--1. Tạo View đưa ra danh sách các sách được bán trong tháng 5 năm 2014
+create view ThiThu1 as
+select tSach.* from tSach join tChiTietHDB on tSach.MaSach = tChiTietHDB.MaSach join tHoaDonBan on tChiTietHDB.SoHDB = tHoaDonBan.SoHDB
+where month(tHoaDonBan.NgayBan) = 5 and year(tHoaDonBan.NgayBan) = 2014
+
+select * from ThiThu1
+
+--2. Tạo thủ tục có đầu vào là năm, đầu ra là số lượng sách nhập, sách bán và sách tồn của năm đó
+create procedure ThiThu2
+@nam int, @slsn int output, @slsb int output, @slst int output
+as
+begin
+	declare @sosachhienco int
+	select @slsn = sum(tChiTietHDN.SLNhap) from tHoaDonNhap join tChiTietHDN on tHoaDonNhap.SoHDN = tChiTietHDN.SoHDN
+	where year(tHoaDonNhap.NgayNhap) = @nam
+	select @slsb = sum(tChiTietHDB.SLBan) from tHoaDonBan join tChiTietHDB on tHoaDonBan.SoHDB = tChiTietHDB.SoHDB
+	where year(tHoaDonBan.NgayBan) = @nam
+	select @sosachhienco = sum(SoLuong) from tSach
+	select @slst = @sosachhienco + @slsn - @slsb
+end
+
+declare @slsn int, @slsb int, @slst int
+exec ThiThu2 2014, @slsn out, @slsb out, @slst out
+print @slsn
+print @slsb
+print @slst
+
+--3. Tạo hàm đưa ra thông tin Nhân viên sinh nhật trong ngày là tham số nhập vào
+create function ThiThu3
+(@ns nvarchar(20))
+returns table
+as return
+(
+	select * from tNhanVien where NgaySinh = @ns
+)
+
+select * from ThiThu3('1990-09-11')
+
+--4. Thêm trường Số lượng sách và Tổng tiền hàng vào bảng nhà cung cấp, cập nhật dữ liệu cho trường này mỗi khi nhập hàng (gồm cả thêm, sửa, xóa).
+alter table tNhaCungCap add SoLuongSach int
+alter table tNhaCungCap add TongTienHang int
+
+create trigger ThiThu4 on tChiTietHDN
+for insert, update, delete
+as
+begin
+	declare @shdnin nvarchar(10), @shdnde nvarchar(10), @msin nvarchar(10), @msde nvarchar(10), @slnin int, @slnde int, @dgin int, @dgde int, @mnccin nvarchar(10), @mnccde nvarchar(10)
+	select @shdnin = SoHDN, @msin = MaSach, @slnin = SLNhap from inserted
+	select @shdnde = SoHDN, @msde = MaSach, @slnde = SLNhap from deleted
+	select @mnccin = MaNCC from tHoaDonNhap where SoHDN = @shdnin
+	select @mnccde = MaNCC from tHoaDonNhap where SoHDN = @shdnde
+	select @dgin = DonGiaNhap from tSach where MaSach = @msin
+	select @dgde = DonGiaNhap from tSach where MaSach = @msde
+	update tNhaCungCap set SoLuongSach = isnull(SoLuongSach,0) + @slnin where MaNCC = @mnccin
+	update tNhaCungCap set TongTienHang = isnull(TongTienHang,0) + @dgin * @slnin where MaNCC = @mnccin
+	update tNhaCungCap set SoLuongSach = isnull(SoLuongSach,0) - @slnde where MaNCC = @mnccde
+	update tNhaCungCap set TongTienHang = isnull(TongTienHang,0) - @dgde * @slnde where MaNCC = @mnccde
+end
+
+select * from tHoaDonNhap
+select * from tChiTietHDN
+select * from tNhaCungCap
+insert into tChiTietHDN values('HDN01','S04',20,NULL)
+update tChiTietHDN set SLNhap = 10 where MaSach = 'S04'
+delete from tChiTietHDN where MaSach = 'S03'
+
+--5. Tạo login TranVietAnh, tạo user TranVietAnh cho TranVietAnh trên CSDL QLBanSach
+exec sp_addlogin TranVietAnh,123
+exec sp_adduser TranVietAnh,TranVietAnh
+--Phân quyền Select trên view ở câu 1 cho TranVietAnh và TranVietAnh được phép phân quyền cho người khác
+grant select on ThiThu1 to TranVietAnh with grant option
+--Đăng nhập TranVietAnh để kiểm tra
+--Tạo login NguyenCongHieu, tạo user NguyenCongHieu cho NguyenCongHieu trên CSDL QLBanSach
+exec sp_addlogin NguyenCongHieu,123
+exec sp_adduser NguyenCongHieu,NguyenCongHieu
+--Đăng nhập NguyenCongHieu để kiểm tra
+--Từ login TranVietAnh, phân quyền Select trên view Câu 1 cho NguyenCongHieu
+grant select on ThiThu1 to NguyenCongHieu
+--Đăng nhập NguyenCongHieu để kiểm tra
+--6. Tạo View danh sách Nhân viên xuất sắc của tháng 5/2014 bao gồm các nhân viên có tổng hóa đơn nhập và bán là cao nhất trong tháng đó
+create view ThiThu6 as
+select top 1 with ties MaNV, TenNV, TongSoHoaDon from tNhanVien order by TongSoHoaDon desc
+update tNhanVien
+set TongSoHoaDon = isnull(b.SHDB,0)
+from
+(select count(distinct SoHDB) as SHDB, MaNV from tHoaDonBan where month(NgayBan) = 5 and year(NgayBan) = 2014 group by MaNV) b
+where tNhanVien.MaNV = b.MaNV 
+update tNhanVien
+set TongSoHoaDon = isnull(n.SHDN,0)
+from
+(select count(distinct SoHDN) as SHDN, MaNV from tHoaDonNhap where month(NgayNhap) = 5 and year(NgayNhap) = 2014 group by MaNV) n
+where tNhanVien.MaNV = n.MaNV 
---
+++ ThucHanhTrenLop.sql
@@ -0,0 +1,240 @@
+﻿	use BT1CSDL
+go
+
+--Thủ tục
+--1. Tạo thủ tục có đầu vào là mã sách, đầu ra là số lượng sách đó được bán trong năm 2014
+create procedure C1
+@ms nvarchar(20), @sl int output
+as
+begin
+select @sl = (select sum(tChiTietHDB.SLBan) from tChiTietHDB join tHoaDonBan on tChiTietHDB.SoHDB = tHoaDonBan.SoHDB
+where tChiTietHDB.MaSach = @ms and year(tHoaDonBan.NgayBan) = 2014 group by tChiTietHDB.MaSach)
+end;
+
+declare @sl int
+exec C1 'S01', @sl output
+print @sl
+
+--2. Tạo thủ tục có đầu vào là ngày, đầu ra là số lượng hóa đơn và số tiền bán của sách trong ngày đó
+create procedure C2
+@ngay nvarchar(20), @slhd int output, @stb int output
+as
+begin
+select @slhd = (select count(tHoaDonBan.SoHDB) from tHoaDonBan where NgayBan = @ngay)
+select @stb = (select sum(tChiTietHDB.SLBan * tSach.DonGiaBan) from tHoaDonBan join tChiTietHDB on tHoaDonBan.SoHDB = tChiTietHDB.SoHDB
+join tSach on tChiTietHDB.MaSach = tSach.MaSach where tHoaDonBan.NgayBan = @ngay)
+end;
+
+declare @slhd int, @stb int
+exec C2 '2014-8-11', @slhd output, @stb output
+print N'Số lượng hóa đơn: ' + case when @slhd > 0 then cast(@slhd as nvarchar) else '0' end
+print N'Số tiền bán: ' + case when @stb > 0 then cast(@stb as nvarchar) else '0' end
+
+--3. Tạo thủ tục có đầu vào là mã nhà cung cấp, đầu ra là số đầu sách và số tiền cửa hàng đã nhập của nhà cung cấp đó
+create procedure C3
+@mncc nvarchar(10), @ss int output, @stn int output
+as
+begin
+select @ss = (select sum(tChiTietHDN.SLNhap) from tHoaDonNhap join tChiTietHDN on tHoaDonNhap.SoHDN = tChiTietHDN.SoHDN
+where tHoaDonNhap.MaNCC = @mncc)
+select @stn = (select sum(tChiTietHDN.SLNhap * tSach.DonGiaNhap) from tHoaDonNhap join tChiTietHDN on tHoaDonNhap.SoHDN = tChiTietHDN.SoHDN
+join tSach on tChiTietHDN.MaSach = tSach.MaSach where tHoaDonNhap.MaNCC = @mncc)
+end;
+
+declare @ss int, @stn int
+exec C3 N'NCC01', @ss output, @stn output
+print N'Số sách nhập: ' + case when @ss > 0 then cast(@ss as nvarchar) else '0' end
+print N'Số tiền nhập: ' + case when @stn > 0 then cast(@stn as nvarchar) else '0' end
+
+--4 Tạo thủ tục có đầu vào là năm, đầu ra là số tiền nhập hàng, số tiền bán hàng của năm đó.
+create procedure STNB 
+@nam nvarchar(50), @STB int output, @STN int output
+as 
+begin
+select @STB = (select sum(tChiTietHDB.SLBan * tSach.DonGiaBan) from tSach inner join tChiTietHDB on tSach.MaSach = tChiTietHDB.MaSach
+				inner join tHoaDonBan on tChiTietHDB.SoHDB = tHoaDonBan.SoHDB where year(tHoaDonBan.NgayBan) = @nam)
+select @STN = (select sum(tChiTietHDN.SLNhap * tSach.DonGiaNhap) from tSach inner join tChiTietHDN on tSach.MaSach = tChiTietHDN.MaSach
+				inner join tHoaDonNhap on tChiTietHDN.SoHDN = tHoaDonNhap.SoHDN where year(tHoaDonNhap.NgayNhap) = @nam)
+end;
+
+declare @STB int, @STN int
+exec STNB '2014', @STB output, @STN output
+print N'Số tiền bán: ' + case when @STB > 0 then cast(@STB as nvarchar) else '0' end
+print N'Số tiền nhập: ' + case when @STN > 0 then cast(@STN as nvarchar) else '0' end
+--5. Tạo thủ tục có đầu vào là mã NXB, đầu ra là số lượng sách tồn của nhà xuất bản đó
+create procedure C5
+@mnxb nvarchar(10), @slst int output
+as
+begin
+declare @tsl int, @slb int
+select @tsl = (select sum(tSach.SoLuong) from tSach join tNhaXuatBan on tSach.MaNXB = tNhaXuatBan.MaNXB
+where tNhaXuatBan.MaNXB = @mnxb)
+select @slb = (select sum(tSach.SoLuong) from tSach join tNhaXuatBan on tSach.MaNXB = tNhaXuatBan.MaNXB
+where tSach.MaSach not in (select tChiTietHDB.MaSach from tChiTietHDB join tHoaDonBan on tChiTietHDB.SoHDB = tHoaDonBan.SoHDB)
+and tNhaXuatBan.MaNXB = @mnxb)
+select @slst = @tsl - @slb
+end;
+
+declare @slst int
+exec C5 'NXB09', @slst output
+print N'Số lượng sách tồn: ' + case when @slst > 0 then cast(@slst as nvarchar) else '0' end
+
+--6. Tạo thủ tục nhập dữ liệu cho bảng hóa đơn nhập và chi tiết hóa đơn nhập cùng lúc (sử dụng transaction)
+
+--7. Tạo thủ tục xóa đồng thời hóa đơn bán và chi tiết hóa đơn bán (dùng transaction)
+
+--8. Tạo thủ tục có đầu vào là năm, đầu ra là số lượng sách nhập, sách bán và sách tồn của năm đó
+create procedure C8
+@nam int, @sn int output, @sb int output, @st int output
+as
+begin
+declare @tsl int
+select @sn = (select sum(tChiTietHDN.SLNhap) from tChiTietHDN join tHoaDonNhap on tChiTietHDN.SoHDN = tHoaDonNhap.SoHDN
+where year(tHoaDonNhap.NgayNhap) = @nam)
+select @sb = (select sum(tChiTietHDB.SLBan) from tChiTietHDB join tHoaDonBan on tChiTietHDB.SoHDB = tHoaDonBan.SoHDB
+where year(tHoaDonBan.NgayBan) = @nam)
+select @tsl = (select sum(tSach.SoLuong) from tSach)
+select @st = @tsl - @sb
+end;
+
+declare @sn int, @sb int, @st int
+exec C8 2014,@sn output, @sb output, @st output
+print N'Số lượng sách nhập: ' + case when @sn > 0 then cast(@sn as nvarchar) else '0' end
+print N'Số lượng sách bán: ' + case when @sb > 0 then cast(@sb as nvarchar) else '0' end
+print N'Số lượng sách tồn: ' + case when @st > 0 then cast(@st as nvarchar) else '0' end
+
+
+--Hàm
+--10. Tạo hàm đưa ra tổng số tiền đã nhập sách trong một năm với tham số đầu vào là năm
+create function C10 
+(@nam int)
+returns int
+as
+begin
+declare @tstns int
+set @tstns = (select sum(tChiTietHDN.SLNhap * tSach.DonGiaNhap) from tSach join tChiTietHDN on tSach.MaSach = tChiTietHDN.MaSach
+join tHoaDonNhap on tChiTietHDN.SoHDN = tHoaDonNhap.SoHDN where year(tHoaDonNhap.NgayNhap) = @nam)
+return @tstns
+end
+
+select dbo.C10(2014) TSTNS
+
+--11. Tạo hàm đưa ra danh sách 10 đầu sách bán chạy nhất trong tháng nào đó (tháng là tham số đầu vào)
+create function C11
+(@thang int, @nam int)
+returns table 
+as return 
+(
+	select * from tSach
+	where tSach.MaSach in (select top 10 tChiTietHDB.MaSach from tChiTietHDB join tHoaDonBan on tChiTietHDB.SoHDB = tHoaDonBan.SoHDB
+	where MONTH(tHoaDonBan.NgayBan) = @thang and YEAR(tHoaDonBan.NgayBan) = @nam group by tChiTietHDB.MaSach order by 
+	sum(tChiTietHDB.SLBan) desc)
+)
+select c.TenSach from dbo.C11(5,2014) c
+--12. Tạo hàm đưa ra danh sách n nhân viên có doanh thu cao nhất trong một năm với n và năm là tham số đầu vào
+create function C12
+(@n int, @nam int)
+returns table
+as return 
+(
+	select * from tNhanVien
+	where tNhanVien.MaNV in (select top (@n) tHoaDonBan.MaNV from tHoaDonBan join tChiTietHDB on tHoaDonBan.SoHDB = tChiTietHDB.SoHDB
+	join tSach on tChiTietHDB.MaSach = tSach.MaSach where year(tHoaDonBan.NgayBan) = @nam group by tHoaDonBan.MaNV
+	order by sum(tChiTietHDB.SLBan * tSach.DonGiaBan) desc)
+)
+
+select * from dbo.C12(4,2014)
+
+--13. Tạo hàm đưa ra thông tin khách hàng sinh nhật trong ngày là tham số nhập vào
+create function C13
+(@sn nvarchar(20))
+returns table 
+as return
+(
+	select * from tKhachHang where NgaySinh = @sn
+)
+
+select * from tKhachHang
+update tKhachHang
+set NgaySinh = '2001-5-17' 
+where MaKH = 'KH01'
+
+select * from dbo.C13('2001-5-17')
+
+--14. Tạo hàm đưa ra danh sách tồn trong kho quá 2 năm 
+create function C14()
+returns table
+as return
+(
+	select * from tSach where tSach.MaSach not in (select tChiTietHDB.MaSach from tChiTietHDB
+	join tHoaDonBan on tChiTietHDB.SoHDB = tHoaDonBan.SoHDB
+	where YEAR(getdate()) - year(NgayBan) < 2)
+)
+
+select * from C14()
+
+alter table tChiTietHDB
+add DonGia money
+
+alter table tKhachHang
+add TongHoaDon money
+select * from tKhachHang
+create trigger DonGia on tChiTietHDB
+for insert, update as
+begin
+declare @gia money, @masach nvarchar(15), @sohdb nvarchar(10)
+select @masach = MaSach, @sohdb = SoHDB from inserted
+select @gia = tSach.DonGiaBan from tSach where MaSach = @masach
+update tChiTietHDB set DonGia = @gia where MaSach = @masach and SoHDB = @sohdb
+end
+
+create trigger C33_trigger on tHoaDonBan
+for insert as
+begin
+	declare @makh nvarchar(10)
+	select @makh = MaKH from inserted
+	update tKhachHang set SoLuong = isnull(SoLuong,0) + 1 where MaKH = @makh
+end
+
+create trigger C4_trigger on tHoaDonBan
+for insert, update, delete
+as
+begin
+	declare @makh nvarchar(10)
+	select @makh = MaKH from inserted
+	update tKhachHang set SoLuong = isnull(SoLuong,0) + 1 where MaKH = @makh
+	select @makh = MaKH from deleted
+	update tKhachHang set SoLuong = isnull(SoLuong,0) - 1 where MaKH = @makh
+end
+
+select * from tKhachHang where MaKH = 'KH04'
+insert into tHoaDonBan values ('HDB32', 'NV02', '2014-08-11', 'KH04', null)
+select * from tKhachHang where MaKH = 'KH04'
+delete from tChiTietHDB where SoHDB in (select SoHDB from tHoaDonBan where MaKH = 'KH04')
+delete from tHoaDonBan where MaKH = 'KH04'
+select * from tKhachHang where MaKH = 'KH04'
+
+create trigger C5_triiger on tChiTietHDB
+for insert, update, delete
+as
+begin
+declare @sohd nvarchar(10), @sosp int
+select @sohd = SoHDB, @sosp = sum(SLBan) group by SoHDB from inserted
+update tHoaDonBan set SoSanPham = 
+end
+
+
+select * from tHoaDonBan
+
+create trigger C4TreLop_trigger on tHoaDonBan
+for insert, update, delete
+as
+begin
+declare @MaKH1 nvarchar(10), @MaKH2 nvarchar(10), @in int, @de int
+select @MaKH1 = MaKH, @in = 1 from inserted
+select @MaKH2 = MaKH, @in = 1 from deleted
+update tKhachHang set SoLuong = isnull(SoLuong,0) + isnull(@in,0) where MaKH = @MaKH1
+update tKhachHang set SoLuong = isnull(SoLuong,0) - isnull(@de,0) where MaKH = @MaKH2
+end
+
+
